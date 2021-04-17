449da00ee50c72bd69aa9e8e1e79e32d4b56acfd
Ettore Di Giacinto <mudler@sabayon.org>
2021-04-17 15:10:04 +0200

    Update entity apply

--- packages/entities-apply/entity-apply.sh
+++ packages/entities-apply/entity-apply.sh
@@ -3,12 +3,18 @@
 entity_name="$1"
 
 if [ -d "/etc/entities/$entity_name/group/" ]; then
+    if [ ! -e /etc/group ]; then
+        touch /etc/group
+    fi
     for filename in /etc/entities/$entity_name/group/*.yaml; do
         entities apply -f /etc/group $filename
     done
 fi
 
 if [ -d "/etc/entities/$entity_name/passwd/" ]; then
+    if [ ! -e /etc/passwd ]; then
+        touch /etc/passwd
+    fi
     for filename in /etc/entities/$entity_name/passwd/*.yaml; do
         entities apply -f /etc/passwd $filename
     done
