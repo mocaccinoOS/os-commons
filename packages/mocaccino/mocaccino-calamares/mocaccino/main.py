23098840928d9c85ba3e099e8ce3ab8283795e47
Joost Ruis <joost.ruis@sabayon.org>
2021-06-20 01:37:34 +0200

    mocaccino/mocaccino-calamares: change remove order We really do not want to have mocaccino/live-setup installed after installation. This keeps adding entries to e.g. /etc/gdm/custom.conf for mocaccino user to autologin and what else.

--- packages/mocaccino/mocaccino-calamares/mocaccino/main.py
+++ packages/mocaccino/mocaccino-calamares/mocaccino/main.py
@@ -16,8 +16,8 @@ RE_IS_COMMENT = re.compile("^ *#")
 luet_packages2remove = [
     "system/mocaccino-calamares",
     "apps/calamares",
-    "repository/livecd",
     "mocaccino/live-setup",
+    "repository/livecd",
 ]
 
 
